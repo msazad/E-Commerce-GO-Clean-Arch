33f0862eb81b627c717946811f43bb230ebc6cdd
user <msazad202@gmail.com>
2024-01-06 22:10:52 +0530

    invnetory added

--- pkg/repository/coupon.go
+++ pkg/repository/coupon.go
@@ -1,6 +1,7 @@
 package repository
 
 import (
+	"errors"
 	"fmt"
 
 	"github.com/msazad/go-Ecommerce/pkg/domain"
@@ -8,40 +9,73 @@ import (
 	"gorm.io/gorm"
 )
 
-type couponRepository struct{
+type couponRepository struct {
 	db *gorm.DB
 }
 
-func NewCouponRepository(DB *gorm.DB)interfaces.CouponRepository{
+func NewCouponRepository(DB *gorm.DB) interfaces.CouponRepository {
 	return &couponRepository{
-		db:DB,
+		db: DB,
 	}
 }
 
-func (c *couponRepository)AddCoupon(coup domain.Coupon)error{
-	if err:=c.db.Exec("INSERT INTO coupons(name,discount_rate,valid) values($1,$2,$3)",coup.Name,coup.DiscountRate,coup.Valid).Error;err!=nil{
+func (c *couponRepository) AddCoupon(coup domain.Coupon) error {
+	if err := c.db.Exec("INSERT INTO coupons(name,discount_rate,valid) values($1,$2,$3)", coup.Name, coup.DiscountRate, coup.Valid).Error; err != nil {
 		return err
 	}
 	return nil
 }
 
-func (c *couponRepository)MakeCouponInvalid(id int)error{
-	if err:=c.db.Exec("UPDATE coupons SET valid=false where id=$1",id).Error;err!=nil{
+func (c *couponRepository) MakeCouponInvalid(id int) error {
+	if err := c.db.Exec("UPDATE coupons SET valid=false where id=$1", id).Error; err != nil {
 		return err
 	}
 
 	return nil
 }
-func (c *couponRepository)FindCouponDiscount(coupon string)int{
+func (c *couponRepository) FindCouponDiscount(coupon string) int {
 	var discountRate int
-	err:=c.db.Raw("select discount_rate from coupons where name=?",coupon).Scan(&discountRate).Error
-	if err!=nil{
+	err := c.db.Raw("select discount_rate from coupons where name=?", coupon).Scan(&discountRate).Error
+	if err != nil {
 		return 0
 	}
 	// if !coupon.Valid {
 	// return 1
-    // }
-	fmt.Println("Discount:",discountRate)
+	// }
+	fmt.Println("Discount:", discountRate)
 	return discountRate
 }
-//
+
+func (c *couponRepository) GetCoupons(page, limit int) ([]domain.Coupon, error) {
+	if page == 0 {
+		page = 1
+	}
+	if limit == 0 {
+		limit = 10
+	}
+	offset := (page - 1) * limit
+	var coupons []domain.Coupon
+
+	if err := c.db.Raw("select id,name,discount_rate,valid from coupons limit ? offset ?", limit, offset).Scan(&coupons).Error; err != nil {
+		return []domain.Coupon{}, err
+	}
+	return coupons, nil
+}
+
+func (c *couponRepository) ValidateCoupon(coupon string) (bool, error) {
+	count := 0
+	if err := c.db.Raw("select count(id) from coupons where name=?", coupon).Scan(&count).Error; err != nil {
+		return false, err
+	}
+	if count < 1 {
+		return false, errors.New("not a valid coupon")
+	}
+	valid := true
+	if err := c.db.Raw("select valid from coupons where name=?", coupon).Scan(&valid).Error; err != nil {
+		return false, err
+	}
+	if !valid {
+		return false, errors.New("not a valid coupon")
+	}
+	return true,nil
+}
---
+++ pkg/repository/inventory.go
@@ -0,0 +1,251 @@
+package repository
+
+import (
+	"errors"
+	"strconv"
+
+	"github.com/msazad/go-Ecommerce/pkg/repository/interfaces"
+	"github.com/msazad/go-Ecommerce/pkg/utils/models"
+	"gorm.io/gorm"
+)
+
+type inventoryRepository struct {
+	DB *gorm.DB
+}
+
+func NewInventoryRepository(DB *gorm.DB) interfaces.InventoryRepository {
+	return &inventoryRepository{
+		DB: DB,
+	}
+}
+
+func (i *inventoryRepository) AddInventory(inventory models.Inventory, url string) (models.InventoryResponse, error) {
+	var inventoryResponse models.InventoryResponse
+	query := `
+	INSERT INTO inventories (category_id,product_name,description,stock,price,image)
+	VALUES (?,?,?,?,?,?)RETURNING id
+	`
+	i.DB.Raw(query, inventory.CategoryID, inventory.ProductName, inventory.Description, inventory.Stock, inventory.Price, url).Scan(&inventoryResponse.ProductID)
+	return inventoryResponse, nil
+}
+
+func (i *inventoryRepository) UpdateImage(invID int, url string) (models.Inventory, error) {
+	//Check the database connection
+	if i.DB == nil {
+		return models.Inventory{}, errors.New("database connection is nil")
+	}
+	if err := i.DB.Exec("UPDATE inventories SET image=? WHERE id=?", url, invID).Error; err != nil {
+		return models.Inventory{}, err
+	}
+	//Retrieve the update
+	var inventory models.Inventory
+	if err := i.DB.Raw("SELECT * FROM inventories WHERE id=?", url, invID).Scan(&inventory).Error; err != nil {
+		return models.Inventory{}, err
+	}
+	return inventory, nil
+}
+
+func (i *inventoryRepository) CheckInventory(pid int) (bool, error) {
+	var k int
+	err := i.DB.Raw("SELECT COUNT(*) FROM inventories WHERE id=?", pid).Scan(&k).Error
+	if err != nil {
+		return false, err
+	}
+
+	if k == 0 {
+		return false, err
+	}
+	return true, err
+}
+
+func (i *inventoryRepository) UpdateInventory(pid int, invData models.UpdateInventory) (models.Inventory, error) {
+
+	//Check the database collection
+	if i.DB == nil {
+		return models.Inventory{}, errors.New("database connection is nil")
+	}
+
+	if invData.CategoryID != 0 {
+		if err := i.DB.Exec("UPDATE inventories SET category_id=? WHERE id=?", invData.CategoryID, pid).Error; err != nil {
+			return models.Inventory{}, err
+		}
+	}
+	if invData.ProductName != "" && invData.ProductName != "string" {
+		if err := i.DB.Exec("UPDATE inventories SET product_name =? WHERE id=?", invData.ProductName, pid).Error; err != nil {
+			return models.Inventory{}, err
+		}
+	}
+	if invData.Stock != 0 {
+		if err := i.DB.Exec("UPDATE inventories SET stock =? WHERE id=?", invData.Stock, pid).Error; err != nil {
+			return models.Inventory{}, err
+		}
+	}
+	if invData.Price != 0 {
+		if err := i.DB.Exec("UPDATE inventories SET price=?WHERE id=?", invData.Price, pid).Error; err != nil {
+			return models.Inventory{}, err
+		}
+	}
+	//Retrieve the update
+	var inventory models.Inventory
+	if err := i.DB.Raw("SELECT * FROM inventories WHERE id=?", pid).Scan(&inventory).Error; err != nil {
+		return models.Inventory{}, err
+	}
+
+	return inventory, nil
+}
+
+func (i *inventoryRepository) DeleteInventory(inventoryID string) error {
+	id, err := strconv.Atoi(inventoryID)
+	if err != nil {
+		return errors.New("converting into integer not hapened")
+	}
+
+	result := i.DB.Exec("DELETE FROM inventories WHERE id=?", id)
+
+	if result.RowsAffected < 1 {
+		return errors.New("no records with that ID exist")
+	}
+	return nil
+}
+
+func (i *inventoryRepository) ShowIndividualProducts(id string) (models.Inventory, error) {
+	pid, error := strconv.Atoi(id)
+	if error != nil {
+		return models.Inventory{}, errors.New("convertion not happened")
+	}
+	var product models.Inventory
+	err := i.DB.Raw(`
+	SELECT
+	    *
+		FROM
+		    inventories
+		WHERE
+		    inventories.id=?
+			`, pid).Scan(&product).Error
+	if err != nil {
+		return models.Inventory{}, errors.New("error retrieved record")
+	}
+	return product, nil
+}
+
+func (ad *inventoryRepository) ListProducts(page int, limit int) ([]models.InventoryList, error) {
+	//pagination purpose -
+	if page == 0 {
+		page = 1
+	}
+	if limit == 0 {
+		limit = 10
+	}
+	offset := (page - 1) * limit
+	var productDetails []models.InventoryList
+
+	if err := ad.DB.Raw("SELECT inventories.id,inventories.product_name,inventories.description,inventories.stock,inventories.image,categories.category AS category FROM inventories JOIN categories ON inventories.category_id=categories.id LIMIT ? OFFSET ?", limit, offset).Scan(&productDetails).Error; err != nil {
+		return []models.InventoryList{}, err
+	}
+	return productDetails, nil
+}
+
+func (i *inventoryRepository) CheckStock(pid int) (int, error) {
+	var k int
+	if err := i.DB.Raw("SELECT stock FROM inventories WHERE id=?", pid).Scan(&k).Error; err != nil {
+		return 0, err
+	}
+	return k, nil
+}
+
+func (i *inventoryRepository) CheckPrice(pid int) (float64, error) {
+	var k float64
+	err := i.DB.Raw("SELECT price FROM inventories WHERE id=?", pid).Scan(&k).Error
+	if err != nil {
+		return 0, err
+	}
+	return k,nil
+}
+
+func (ad *inventoryRepository)SearchProducts(key string,page,limit int)([]models.InventoryList,error){
+	if page ==0{
+		page =1
+	}
+	if limit==0{
+		limit=10
+	}
+	offset:=(page-1)*limit
+	var productDetails []models.InventoryList
+
+	query:=`
+	SELECT
+	      inventories.id,
+		  inventories,product_name,
+		  inventories.description,
+		  inventories.stock,
+		  inventories.price,
+		  inventories.image,
+		  categories.category AS category
+	FROM
+	      inventories
+	JOIN
+	      categories
+	ON
+	      inventories.category_id=categories.id
+	WHERE
+	product_name ILIKE '%' ||?||'%'
+	OR description ILIKE '%'||?||'%'
+	LIMIT ? OFFSET ?
+	`
+	if err:=ad.DB.Raw(query,key,limit,offset).Scan(&productDetails).Error;err!=nil{
+		return []models.InventoryList{},err
+	}
+	return productDetails,nil
+}
+
+func (ad *inventoryRepository)GetCategoryProducts(catID int,page,limit int)([]models.InventoryList,error){
+	if page ==0{
+		page=1
+	}
+	if limit==0{
+		limit=10
+	}
+	offset:=(page-1)*limit
+	var productDetails []models.InventoryList
+
+	query :=`
+	SELECT inventories.id,inventories.product_name,inventories.description,inventories.stock,inventories.price,inventories.image,categories.category AS category FROM inventories JOIN categories ON inventories.category_id=categories.id
+	      WHERE inventories.category_id=?
+		  limit ? offset ?
+	`
+
+	if err:=ad.DB.Raw(query,catID,limit,offset).Scan(&productDetails).Error;err!=nil{
+		return []models.InventoryList{},err
+	}
+
+	return productDetails,nil
+}
+
+func (ad *inventoryRepository)AddImage(product_id int,imageURL string)(models.InventoryResponse,error){
+	var inventoryResponse models.InventoryResponse
+	query :=`
+	INSERT INTO images (inventory_id,image_url)
+	VALUES (?,?) RETURNING inventory_id
+	`
+
+	ad.DB.Raw(query,product_id,imageURL).Scan(&inventoryResponse.ProductID)
+
+	return inventoryResponse,nil
+}
+
+func (ad *inventoryRepository)DeleteImage(product_id,imageID int)error{
+	result:=ad.DB.Exec("DELETE FROM images WHERE id=?",imageID)
+
+	if result.RowsAffected<1{
+		return errors.New("no records with that ID exist")
+	}
+	return nil
+}
+
+func (ad *inventoryRepository)GetImagesFromInventoryID(product_id int)([]models.ImageInfo,error){
+	var images []models.ImageInfo
+	if err:=ad.DB.Raw("select id,image_url from images where inventory_id=?",product_id).Scan(&images).Error;err!=nil{
+		return []models.ImageInfo{},err
+	}
+	return images,nil
+}
