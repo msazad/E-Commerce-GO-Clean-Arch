92c16084d75af2b743602d42c610633cea00384c
user <msazad202@gmail.com>
2024-01-19 22:02:06 +0530

    config completed

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -1,6 +1,11 @@
 package config
 
-import "github.com/spf13/viper"
+import (
+	"log"
+
+	"github.com/joho/godotenv"
+	"github.com/spf13/viper"
+)
 
 type Config struct {
 	DBHost     string `mapstructure:"DB_HOST"`
@@ -24,4 +29,25 @@ var envs = []string{
 	"DB_SERVICEID",
 	"DB_AUTHTOKEN",
 	"UNIDOC_LICENSE_API_KEY",
+}
+func LoadConfig()(Config,error){
+	var config Config
+	viper.AddConfigPath("./")
+	viper.SetConfigFile(".env")
+	viper.ReadInConfig()
+	viper.AutomaticEnv()
+
+	for _,env:=range envs{
+		if err:=viper.BindEnv(env);err!=nil{
+			return config,err
+		}
+	}
+	err:=godotenv.Load()
+	if err!=nil{
+		log.Fatal("error while loading the env file")
+	}
+	if err:=viper.Unmarshal(&config);err!=nil{
+		return config,err
+	}
+	return config,nil
 }
