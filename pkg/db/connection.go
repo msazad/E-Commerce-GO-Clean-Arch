8a5b9ae08193d886cbe0125c0a0dcb515f9bc3be
user <msazad202@gmail.com>
2024-01-25 23:11:07 +0530

    usecase

--- pkg/db/connection.go
+++ pkg/db/connection.go
@@ -9,17 +9,16 @@ import (
 	"gorm.io/gorm"
 )
 
-func ConnectDB(cfg config.Config)(*gorm.DB,error){
-	psqlInfo:=fmt.Sprintf("host=%s user=%s password=%s port=%s dbname=%s",cfg.DBHost,cfg.DBUser,cfg.DBPassword,cfg.DBPort,cfg.DBName)
-	db,dbErr:=gorm.Open(postgres.Open(psqlInfo),&gorm.Config{
+func ConnectDB(cfg config.Config) (*gorm.DB, error) {
+	psqlInfo := fmt.Sprintf("host=%s user=%s password=%s port=%s dbname=%s", cfg.DBHost, cfg.DBUser, cfg.DBPassword, cfg.DBPort, cfg.DBName)
+	db, dbErr := gorm.Open(postgres.Open(psqlInfo), &gorm.Config{
 		SkipDefaultTransaction: true,
 	})
 	db.AutoMigrate(&domain.Inventory{})
 	db.AutoMigrate(&domain.Category{})
 	db.AutoMigrate(&domain.Admin{})
 	db.AutoMigrate(&domain.User{})
-	db.AutoMigrate(&domain.Cart{})
-	db.AutoMigrate(&domain.wishlist{})
+	db.AutoMigrate(&domain.Wishlist{})
 	db.AutoMigrate(&domain.WishlistItems{})
 	db.AutoMigrate(&domain.Address{})
 	db.AutoMigrate(&domain.Order{})
@@ -31,6 +30,6 @@ func ConnectDB(cfg config.Config)(*gorm.DB,error){
 	db.AutoMigrate(&domain.Wallet{})
 	db.AutoMigrate(&domain.WalletHistory{})
 	db.AutoMigrate(&domain.Image{})
-
 	return db, dbErr
-}//////////
+
+}
