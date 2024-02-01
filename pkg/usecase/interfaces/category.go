3affbfcc67c028ca7304ada1a76c38b34269c786
user <msazad202@gmail.com>
2024-02-01 21:54:36 +0530

    updations

--- pkg/usecase/interfaces/category.go
+++ pkg/usecase/interfaces/category.go
@@ -5,4 +5,5 @@ import "github.com/msazad/go-Ecommerce/pkg/domain"
 type CategoryUsecase interface {
 	AddCategory(category string)(domain.Category,error)
 	UpdateCategory(current,new string)(domain.Category,error)
+	
 }
