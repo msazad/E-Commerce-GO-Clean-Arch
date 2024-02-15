24254ff903e26c80f871f20b12adfdc9d707310f
user <msazad202@gmail.com>
2024-02-15 14:13:21 +0530

    wallet and wishlist modified

--- pkg/domain/wishlist.go
+++ pkg/domain/wishlist.go
@@ -1,18 +1,12 @@
 package domain
 
-// Cart represents the wishlist of user
+// Wishlist of a user
+
 type Wishlist struct {
-	ID     uint `json:"id" gorm:"primarykey"`
-	UserID uint `json:"user_id" gorm:"not null"`
-	User   User `json:"-" gorm:"foreignkey:UserID"`
-}
-
-// LineItems represents products in the wishlist of user
-
-type WishlistItems struct {
 	ID          uint      `json:"id" gorm:"primarykey"`
-	WishlistID  uint      `json:"cart_id" gorm:"not null"`
-	Wishlist    Wishlist  `json:"-" gorm:"foreignkey:WishlistID"`
+	UserID      uint      `json:"user_id" gorm:"not null"`
+	User        User      `json:"-" gorm:"foreignkey:UserID"`
 	InventoryID uint      `json:"inventory_id" gorm:"not null"`
-	Inventory   Inventory `json:"-" gorm:"foreignkey:InventoryID"`
+	Inventories Inventory `json:"-" gorm:"foreignkey:InventoryID"`
+	IsDeleted   bool      `json:"is_deleted" gorm:"default:false"`
 }
