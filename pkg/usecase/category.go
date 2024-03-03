69d6fc9f8299b6c9714839275012a5f71c4d9399
user <msazad202@gmail.com>
2024-03-03 12:20:04 +0530

    added category and coupon in usecase

---
+++ pkg/usecase/category.go
@@ -0,0 +1,62 @@
+package usecase
+
+import (
+	"errors"
+	"fmt"
+
+	"github.com/msazad/go-Ecommerce/pkg/domain"
+	"github.com/msazad/go-Ecommerce/pkg/repository/interfaces"
+	services "github.com/msazad/go-Ecommerce/pkg/usecase/interfaces"
+)
+
+type categoryUsecase struct {
+	repo interfaces.CategoryRepository
+}
+
+// constructor function
+
+func NewCategoryUsecase(repo interfaces.CategoryRepository) services.CategoryUsecase {
+	return &categoryUsecase{
+		repo: repo,
+	}
+}
+
+func (catU *categoryUsecase) AddCategory(category string) (domain.Category, error) {
+	productResponse, err := catU.repo.AddCategory(category)
+	if err != nil {
+		fmt.Println("error from cat usecase ",err)
+		return domain.Category{}, err
+	}
+	return productResponse, nil
+}
+
+func (catU *categoryUsecase) UpdateCategory(currrent, new string) (domain.Category, error) {
+	result, err := catU.repo.CheckCategory(currrent)
+	if err != nil {
+		return domain.Category{}, err
+	}
+	if !result {
+		return domain.Category{}, errors.New("no category as you mentioned")
+	}
+	newCat, err := catU.UpdateCategory(currrent, new)
+	if err != nil {
+		return domain.Category{}, err
+	}
+	return newCat, nil
+}
+
+func (catU *categoryUsecase) DeleteCategory(categoryId string) error {
+	err := catU.repo.DeleteCategory(categoryId)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (catU *categoryUsecase) GetCategories() ([]domain.Category, error) {
+	categories, err := catU.repo.GetCategories()
+	if err != nil {
+		return []domain.Category{}, errors.New("categories not found")
+	}
+	return categories, nil
+}
---
+++ pkg/usecase/coupon.go
@@ -0,0 +1,44 @@
+package usecase
+
+import (
+	"errors"
+
+	"github.com/msazad/go-Ecommerce/pkg/domain"
+	interfaces "github.com/msazad/go-Ecommerce/pkg/repository/interfaces"
+	services"github.com/msazad/go-Ecommerce/pkg/usecase/interfaces"
+	"github.com/msazad/go-Ecommerce/pkg/utils/models"
+)
+
+type couponUsecase struct {
+	couponRepo interfaces.CouponRepository
+}
+
+// constructor function
+
+func NewCouponUsecase(couponRepo interfaces.CouponRepository) services.CouponUsecase {
+	return &couponUsecase{
+		couponRepo: couponRepo,
+	}
+}
+
+func (coupU *couponUsecase) Addcoupon(coupon models.Coupon) error {
+	if err := coupU.couponRepo.AddCoupon(coupon); err != nil {
+		return errors.New("coupon adding failed")
+	}
+	return nil
+}
+
+func (coupU *couponUsecase) MakeCouponInvalid(id int) error {
+	if err := coupU.couponRepo.MakeCouponInvalid(id); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (coupU *couponUsecase) GetCoupons() ([]domain.Coupon, error) {
+	coupons, err := coupU.couponRepo.GetCoupons()
+	if err != nil {
+		return []domain.Coupon{}, err
+	}
+	return coupons, nil
+}
